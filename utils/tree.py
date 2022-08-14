29c36f785d12ccf64e1c56dbf82467a52c338f57
kaidi.shao <kdroyshao@gmail.com>
2022-08-14 19:26:41 -0400

    Problem 108

---
+++ utils/tree.py
@@ -0,0 +1,11 @@
+# import sys
+# sys.path.append("..")
+# import utils.tree
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
